2f3f6b13070cebd4c3ad7301680f939f9e5e3b2c
tszhong0411 <lainelson411@gmail.com>
2023-05-14 23:06:58 +0800

    feat: remove 'use client' injection

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -12,14 +12,6 @@ export default defineConfig({
   external: ['react'],
 
   async onSuccess() {
-    const files = ['dist/index.js', 'dist/index.mjs']
-
-    for (const file of files) {
-      const content = await fs.promises.readFile(file, 'utf-8')
-      const newContent = `'use client';\n${content}`
-      await fs.promises.writeFile(file, newContent)
-    }
-
     const css = fs.readFileSync('./src/main.css', 'utf-8')
 
     fs.writeFile('./dist/main.css', css, (err) => {
