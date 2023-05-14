65eaff939f789b2cf1fa70cf9b8d53dc5d196c26
tszhong0411 <lainelson411@gmail.com>
2023-05-14 23:37:21 +0800

    feat(ui): add 'use client' injection

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -7,11 +7,19 @@ export default defineConfig({
   treeshake: true,
   splitting: true,
   dts: true,
-  minify: false,
+  minify: true,
   clean: true,
   external: ['react'],
 
   async onSuccess() {
+    const files = ['dist/index.js', 'dist/index.mjs']
+
+    for (const file of files) {
+      const content = await fs.promises.readFile(file, 'utf-8')
+      const newContent = `'use client';\n${content}`
+      await fs.promises.writeFile(file, newContent)
+    }
+
     const css = fs.readFileSync('./src/main.css', 'utf-8')
 
     fs.writeFile('./dist/main.css', css, (err) => {
