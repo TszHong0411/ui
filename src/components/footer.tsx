109e42032b0cdb6fb53e58ab5825f6348ca6ef3b
tszhong0411 <lainelson411@gmail.com>
2023-06-19 19:17:37 +0800

    feat: use english name

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer>
       <div className='mx-auto w-full max-w-7xl px-6 py-4'>
-        &copy; {new Date().getFullYear()} 小康
+        &copy; {new Date().getFullYear()} Hong
       </div>
     </footer>
   )
